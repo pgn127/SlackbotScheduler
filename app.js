61506413130fc94cabf1ab2631dd61f01c448a2a
floresfxf <fflores@colgate.edu>
2017-07-17 16:11:49 -0700

    jay fixed this

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-var fs = require('fs');
-var logger = require('morgan');
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+// var fs = require('fs');
+// var logger = require('morgan');
+// var google = require('googleapis');
+// var googleAuth = require('google-auth-library');
 var express = require('express');
-var request = require('request');
-var client = require('./rtm-client.js')
+// var request = require('request');
+require('./rtm-client');
 var app = express();
 var bodyParser = require('body-parser');
-//var index = require('./routes/index.js');
-app.use(logger('dev'));
+
+// app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-var app = express();
 
-var Slack = require('@slack/client');
-var RtmClient = Slack.RtmClient;
-var RTM_EVENTS = Slack.RTM_EVENTS;
-
-var token;
+const PORT='3001';
 
 
-var CLIENT_ID = '214011705701.213228166656';
-var CLIENT_SECRET = '9ff7b831fa7cc74064e897f5da9563db';
-const PORT=4390;
-
-// Start our server
-app.listen(PORT, function () {
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Example app listening on port " + PORT);
-});
 
 
 // This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
@@ -37,31 +23,31 @@ app.get('/', function(req, res) {
 });
 
 //This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
-app.get('/oauth', function(req, res) {
-    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
-    if (!req.query.code) {
-        res.status(500);
-        res.send({"Error": "Looks like we're not getting code."});
-        console.log("Looks like we're not getting code.");
-    } else {
-        // If it's there...
-
-        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
-        request({
-            url: 'https://slack.com/api/oauth.access', //URL to hit
-            qs: {code: req.query.code, client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET}, //Query string data
-            method: 'GET', //Specify the method
-
-        }, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                res.json(body);
-
-            }
-        })
-    }
-});
+// app.get('/oauth', function(req, res) {
+//     // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+//     if (!req.query.code) {
+//         res.status(500);
+//         res.send({"Error": "Looks like we're not getting code."});
+//         console.log("Looks like we're not getting code.");
+//     } else {
+//         // If it's there...
+//
+//         // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+//         request({
+//             url: 'https://slack.com/api/oauth.access', //URL to hit
+//             qs: {code: req.query.code, client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET}, //Query string data
+//             method: 'GET', //Specify the method
+//
+//         }, function (error, response, body) {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 res.json(body);
+//
+//             }
+//         })
+//     }
+// });
 
 
 
@@ -101,3 +87,9 @@ app.post('/interactive', function(req,res){
 //   res.render('error');
 // });
 // export default app;
+
+// Start our server
+app.listen(PORT, function () {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Example app listening on port " + PORT);
+});
--- rtm-client.js
+++ rtm-client.js
@@ -14,41 +14,24 @@ var messageButtons = {
                     "callback_id": "wopr_game",
                     "color": "#3AA3E3",
                     "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "game",
-                            "text": "Chess",
-                            "type": "button",
-                            "value": "chess"
-                        },
-                        {
-                            "name": "game",
-                            "text": "Falken's Maze",
-                            "type": "button",
-                            "value": "maze"
-                        },
-                        {
-                            "name": "game",
-                            "text": "Thermonuclear War",
-                            "style": "danger",
-                            "type": "button",
-                            "value": "war",
-                            "confirm": {
-                                "title": "Are you sure?",
-                                "text": "Wouldn't you prefer a good game of chess?",
-                                "ok_text": "Yes",
-                                "dismiss_text": "No"
-                            }
-                        }
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    },
+                  },
+
                     ]
                 }
-            ]
-        }
+
+
 
 var RtmClient = require('@slack/client').RtmClient;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-var token = process.env.SLACK_API_TOKEN || 'xoxb-213948372850-8BZYcWtZJvHWTzfZhpTsjGbl';
+var token = process.env.SLACK_API_TOKEN;
+console.log(token);
 
 var rtm = new RtmClient(token);
 rtm.start();
