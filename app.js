eb339973e4bf395815572beaf173163bd840395d
floresfxf <fflores@colgate.edu>
2017-07-17 14:13:41 -0700

    server up

--- app.js
+++ app.js
@@ -3,32 +3,86 @@ var logger = require('morgan');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 var express = require('express');
+var request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var index = require('./routes/index.js');
+//var index = require('./routes/index.js');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+var app = express();
 
 
-app.use('/', index);
+var clientId = '214011705701.213228166656';
+var clientSecret = '9ff7b831fa7cc74064e897f5da9563db';
 
-app.use((req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Instantiates Express and assigns our app variable to it
+
+
+
+// Again, we define a port we want to listen to
+const PORT=4390;
+
+// Lets start our server
+app.listen(PORT, function () {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Example app listening on port " + PORT);
 });
 
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+app.get('/', function(req, res) {
+    res.send('Ngrok is working! Path Hit: ' + req.url);
 });
-export default app;
+
+// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+app.get('/oauth', function(req, res) {
+    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+        console.log("Looks like we're not getting code.");
+    } else {
+        // If it's there...
+
+        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+        request({
+            url: 'https://slack.com/api/oauth.access', //URL to hit
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+            method: 'GET', //Specify the method
+
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+
+            }
+        })
+    }
+});
+
+// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+app.post('/command', function(req, res) {
+    res.send('Your ngrok tunnel is up and running!');
+});
+
+// app.use((req, res, next) => {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+//
+// // error handler
+// app.use((err, req, res, next) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+// export default app;
