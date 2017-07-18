6e2240c7dc2d5f172d89123a89b8579151b51620
floresfxf <fflores@colgate.edu>
2017-07-18 16:16:59 -0700

    commit

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ var models = require('./models');
 var {User} = require('./models');
 var {Reminder} = require('./models');
 var mongoose = require('mongoose');
+var _ = require('underscore');
 
 var slackID;
 
 var {RtmClient, WebClient, CLIENT_EVENTS, RTM_EVENTS} = require('@slack/client');
 var token = process.env.SLACK_API_TOKEN || '';
 
-var rtm = new RtmClient(token);
-var web = new WebClient(token);
-rtm.start();
+//var rtm = new RtmClient(token);
+//var web = new WebClient(token);
+//rtm.start();
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -51,19 +52,55 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 findReminders();
 
+// function findReminders(){
+//   var now = Date.now();
+//   var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).getTime();
+//   Reminder.find({}).where('date').gt(now).lt(tomorrow).exec(function(err,reminders){
+//     if (err){
+//       console.log('error ', err);
+//     }else {
+//       console.log(reminders);
+//       if (reminders){
+//         reminders.forEach(function(reminder){
+//           rtm.sendMessage("UPCOMING REMINDER: " + reminder.subject + ' on ' + new Date(reminder.date), reminder.channelID);
+//         })
+//       }
+//     }
+//   })
+// }
+
 function findReminders(){
   var now = Date.now();
   var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).getTime();
-  Reminder.find({}).where('date').gt(now).lt(tomorrow).exec(function(err,reminders){
+  Reminder.find({}).where('date').gt(now).lt(tomorrow).populate('userID').exec(function(err,reminders){
     if (err){
-      console.log('error ', err);
+      // res.status(400).json({error:err});
+      console.log('error', err);
     }else {
-      console.log(reminders);
-      if (reminders){
-        reminders.forEach(function(reminder){
-          rtm.sendMessage("UPCOMING REMINDER: " + reminder.subject + ' on ' + new Date(reminder.date), reminder.channelID);
-        })
-      }
+        if(reminders){
+            //group the reminders by user id
+            var groupedReminders = _.groupBy(reminders, function(reminder) {
+                console.log(reminder);
+                return reminder.userID._id
+            });
+            console.log(groupedReminders);
+            Object.keys(groupedReminders).forEach(function(user) {
+                var channel;
+                var userReminders = groupedReminders[user];
+                var reminderString = "";
+                console.log('user reminders for ', user, 'are ', userReminders);
+                userReminders.forEach(function(reminder) {
+                    // var dmChannel = rtm.dataStore.getDMByUserId(reminder.userID.slackID);
+                    channel = reminder.channelID;
+                    var date = new Date(reminder.date);
+                    var str = `Reminder: ${date} for ${reminder.subject} \n`;
+                    reminderString+= str;
+                })
+                console.log(reminderString);
+                console.log(channel);
+                rtm.sendMessage(reminderString, channel);
+            })
+        }
     }
   })
 }
