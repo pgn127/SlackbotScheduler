1d77636817f838654babd774c3320309fa897c3e
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-07-17 20:03:13 -0700

    implemented google oauth

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+
 // var fs = require('fs');
 var logger = require('morgan');
-// var google = require('googleapis');
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
 // var googleAuth = require('google-auth-library');
 var express = require('express');
 // var request = require('request');
@@ -19,52 +21,39 @@ var CLIENT_ID = process.env.CLIENT_ID;
 var CLIENT_SECRET = process.env.CLIENT_SECRET;
 const PORT=3000;
 
+//generate the oauth url
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.DOMAIN + '/connect/callback'
+)
+
+const url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  prompt: 'consent',
+  scope: [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/calendar'
+  ],
+  state: encoreURIComponent(JSON.stringify({
+    auth_id: req.query.auth_id
+  }))
+});
+
 // Start our server
 app.listen(PORT, function () {
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Example app listening on port " + PORT);
 });
 
+app.get('/oauth', function(req, res){
+  res.redirect(url);
+})
 
 // This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
 app.get('/', function(req, res) {
     res.send('Ngrok is working! Path Hit: ' + req.url);
 });
-
-//This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
-// app.get('/oauth', function(req, res) {
-//     // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
-//     if (!req.query.code) {
-//         res.status(500);
-//         res.send({"Error": "Looks like we're not getting code."});
-//         console.log("Looks like we're not getting code.");
-//     } else {
-//         // If it's there...
-//
-//         // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
-//         request({
-//             url: 'https://slack.com/api/oauth.access', //URL to hit
-//             qs: {code: req.query.code, client_id: CLIENT_ID, client_secret: CLIENT_SECRET}, //Query string data
-//             method: 'GET', //Specify the method
-//
-//         }, function (error, response, body) {
-//             if (error) {
-//                 console.log(error);
-//             } else {
-//                 res.json(body);
-//
-//             }
-//         })
-//     }
-// });
-
-// function processMessage(message, rtm) {
-//   var locationName = message.text;
-//   var query = (isNaN(locationName) ? 'q=' + locationName : 'zip=' + locationName) + '&units=imperial&APPID=' + WEATHER_API_KEY;
-//   rtm.sendMessage('I\'ll get you the current weather for "' + locationName + '"', message.channel, function() {
-//     // getAndSendCurrentWeather(locationName, query, message.channel, rtm);
-//   });
-// }
 
 // Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
 app.post('/command', function(req, res) {
