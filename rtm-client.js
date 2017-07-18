c7414093a078fc2b15cfdb62f9306f7af7210635
Pam Needle <pneedle@colgate.edu>
2017-07-18 16:57:34 -0700

    Testing if thsi works

--- rtm-client.js
+++ rtm-client.js
@@ -60,6 +60,7 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+    findReminders(rtm);
   var dm = rtm.dataStore.getDMByUserId(message.user); //gets the channel ID for the specific conversation between one user and bot
   const userId = message.user;
   //IF THE USER HAS RESPONDED TO THE PREVIOUS INTERACTIVE MESSAGE, set awaitingResponse tp false again
@@ -171,16 +172,39 @@ function processMessage(message, rtm) {
 }
 
 
-function findReminders(){
+function findReminders(rtm){
   var now = Date.now();
   var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).getTime();
-  Reminder.find({}).where('date').gt(now).lt(tomorrow).exec(function(err,reminders){
+  Reminder.find({}).where('date').gt(now).lt(tomorrow).populate('userID').exec(function(err,reminders){
     if (err){
       // res.status(400).json({error:err});
-      return [];
+      console.log('error', err);
     }else {
-      console.log(reminders);
-      return reminders;
+        if(reminders){
+            //group the reminders by user id
+            var groupedReminders = _.groupBy(reminders, function(reminder) {
+                // console.log('REMINDER IN GROUPEDREMINDER IS', reminder);
+                return reminder.userID._id
+            });
+
+            Object.keys(groupedReminders).forEach(function(user) {
+                var userReminders = groupedReminders[user];
+                var reminderString = "";
+                var channel;
+                // console.log('user reminders for ', user, 'are ', userReminders);
+                userReminders.forEach(function(reminder) {
+                    channel = reminder.channelID;
+                    // var dmChannel = rtm.dataStore.getDMByUserId(reminder.userID.slackID);
+                    var date = new Date(reminder.date);
+                    var str = `Reminder: ${date} for ${reminder.subject} \n`;
+                    reminderString+= str;
+                })
+                // console.log('sending remidner string to user ', reminderString);
+                console.log('rtm issss', rtm);
+                rtm.sendMessage(reminderString, channel);
+            })
+
+        }
     }
   })
 }
