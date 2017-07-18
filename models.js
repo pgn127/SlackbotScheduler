c1a7c3dc9c68f5e2082e866c055511e23075857d
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-07-18 10:22:04 -0700

    created models.js

---
+++ models.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  userID: String,
+  refresh_token: String,
+  access_token: String,
+  auth_id: String,
+  token_type: String,
+  expiry_date: Number
+})
+
+var User = mongoose.model('User', userSchema);
+
+export { User };
