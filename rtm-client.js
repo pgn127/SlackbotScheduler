e6d6a71ad79bd030d5600d7f555ccfc73120eb2e
Pamela Needle <pneedle@Pams-MacBook-Pro.local>
2017-07-19 18:43:42 -0400

    heroku commit

--- rtm-client.js
+++ rtm-client.js
@@ -74,7 +74,7 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   // console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-
+    console.log(message);
   var dm = rtm.dataStore.getDMByUserId(message.user); //gets the channel ID for the specific conversation between one user and bot
   slackID = message.user;
   const userId = message.user;
