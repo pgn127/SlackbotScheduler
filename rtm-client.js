6f93cf80f1a6d2ddf6c0f995779956b046f42fe5
Pamela Needle <pneedle@Pams-MacBook-Pro.local>
2017-07-19 13:07:28 -0700

    heroku commit

--- rtm-client.js
+++ rtm-client.js
@@ -71,7 +71,6 @@ var pamtofrankie = {
 
 }
 
-// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   // console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
