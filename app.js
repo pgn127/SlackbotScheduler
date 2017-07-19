154bad1f7db27d882297d8d22b4192c0e2bfa58d
Pam Needle <pneedle@colgate.edu>
2017-07-19 11:34:44 -0700

    deubgging

--- app.js
+++ app.js
@@ -219,6 +219,7 @@ function checkConflicts(meeting){
     // var meetingStart = meeting.date+'T'+meeting.time+'-00:00';
     var dateSplit = meeting.time.split('-');
     var timeSplit = meeting.time.split(':');
+    console.log(dateSplit, timeSplit);
     var meetingStart = new Date(dateSplit[0], dateSplit[1], dateSplit[2], timeSplit[0], timeSplit[1], timeSplit[2]).toISOString();
     var meetingEnd = new Date(dateSplit[0], dateSplit[1], dateSplit[2], timeSplit[0] + 1, timeSplit[1], timeSplit[2]).toISOString();
 
