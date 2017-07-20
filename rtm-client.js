ce7aa70fca9223094d7e4a582a7248f9981a9d11
Pamela Needle <pneedle@Pams-MacBook-Pro.local>
2017-07-19 18:12:49 -0700

    heroku commit

--- rtm-client.js
+++ rtm-client.js
@@ -282,12 +282,15 @@ function checkConflicts(meeting, rtm){
 
                         var meetingStartTime = (new Date(Date.parse(startTime))).toTimeString();
                         var meetingEndTime = (new Date(Date.parse(endTime))).toTimeString();
+                        var userEventStartTime = (new Date(Date.parse(time.start))).toTimeString();
+                        var userEventEndTime = (new Date(Date.parse(time.end))).toTimeString();
+                        var userEventDate = (new Date(Date.parse(time.start))).toDateString();
 
 
                         if(Date.parse(startTime) >= Date.parse(time.start) && Date.parse(startTime) <= Date.parse(time.end) || Date.parse(endTime) >= Date.parse(time.start) && Date.parse(endTime) <= Date.parse(time.end)){
                         // if(new Date(startTime) >= new Date(time.start) && new Date(startTime) <= new Date(time.end) || new Date(endTime) >= new Date(time.start) && new Date(endTime) <= new Date(time.end)){
 
-                            rtm.sendMessage(`BUSY: the meeting you tried to schedule for day  ${startTime.substring(0, 10)} from \n ${(new Date(Date.parse(startTime))).toLocaleTimeString()}-${(new Date(Date.parse(endTime))).toLocaleTimeString()} \n conflicts with ${invitee}'s event on day ${time.start.substring(0,10)} from \n ${(new Date(Date.parse(time.start))).toTimeString()}-${new Date(time.end).toLocaleTimeString(timezone)}.\n\n`,'D6ATM9WMU');
+                            rtm.sendMessage(`BUSY: the meeting you tried to schedule for day  ${startTime.substring(0, 10)} from \n ${meetingStartTime}-${meetingEndTime} \n conflicts with ${invitee}'s event on day ${time.start.substring(0,10)} from \n ${userEventStartTime}-${new Date(time.end).toLocaleTimeString(timezone)}.\n\n`,'D6ATM9WMU');
 
                         } else {
 
