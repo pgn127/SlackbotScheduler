973634d85fc552f5e36a2964a37e80dd6684cee1
Pamela Needle <pneedle@Pams-MacBook-Pro.local>
2017-07-20 18:29:15 -0700

    heroku commit

--- app.js
+++ app.js
@@ -104,31 +104,9 @@ app.post('/command', function(req, res) {
 app.post('/slack/interactive', function(req,res){
   var payload = JSON.parse(req.body.payload);
   console.log('PAYLOAD ACTIONS', payload);
-  //if the user selects a new meeting time from list of meetings
-  if(payload.actions[0].type === "select"){
-      console.log('the intervative message was a select');
-    //
-    //   var selectedMeeting = payload.actions[0].selected_options[0].value;
-    //   var meetStartTime = selectedMeeting.slice(0,10);
-    //   var meetDate = selectedMeeting.slice(11,19);
-    // //   console.log('selected options attachemnts', payload.original_message.attachments[0].fields[1].value.split(', '));
-    //   var invitees = payload.original_message.attachments[0].fields[1].value.split(', ');
-
-    //   var newMeeting = new Meeting({
-    //       userID: user._id,
-    //       channelID: payload.channel.id,
-    //       subject: meetingSubject,
-    //       date: meetingDate,
-    //       time: meetingTime,
-    //       invitees: meetingInvitees,
-    //   })
-
-  }
-  console.log('PAYLOAD.ACTIONS[0].VALUE ! == FALSE', payload.actions[0].value !== 'false');
   //if the user selects confirm button
   if(payload.actions[0].value !== 'false') {
   // else if(payload.actions[0].type === "button" && payload.actions[0].value !== 'false') {
-    //   console.log('PAYLOAD ACTIONS', payload.actions.selected_options);
     slackID = payload.user.id;
     User.findOne({slackID: slackID}).exec(function(err, user){
       if(err || !user){
@@ -137,8 +115,6 @@ app.post('/slack/interactive', function(req,res){
       } else if (user){
           if(payload.actions[0].type === "select"){
               //meeting with conflicts with select list
-            //   console.log('the intervative message was a select');
-            //   console.log('selected options', payload.actions[0].selected_options[0].value);
               var selectedMeeting = payload.actions[0].selected_options[0].value;
               var meetingTime = selectedMeeting.slice(11,19);
               var meetingDate = selectedMeeting.slice(0,10);
@@ -156,7 +132,7 @@ app.post('/slack/interactive', function(req,res){
           else {
               //it was a meeting that had no conflicts
               var meetingSubject = payload.original_message.attachments[0].fields[0].value;
-              var meetingDate = payload.original_message.attachments[0].fields[2].value; //NOTE: removed Date.parse because conflicts with how we deal with dates in the check conflcits and the date type should be String
+              var meetingDate = payload.original_message.attachments[0].fields[2].value;
               var meetingTime = payload.original_message.attachments[0].fields[3].value;
               var meetingInvitees = payload.original_message.attachments[0].fields[1].value.split(", ");
           }
